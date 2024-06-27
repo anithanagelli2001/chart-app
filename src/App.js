eb5ae5cacefe714e0d357cc2d57a36c1740da6cd
Anitha <anithanagelli082@gmail.com>
2024-06-27 16:58:26 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='App-header'>React Charting Library</h1>
+      <ChartComponent />
+    </div>
+  );
+}
+
+export default App;
